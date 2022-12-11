97442c11c5754e811603e26d927ef7d78819ee6a
ChoiDongHo <goguma1000@ajou.ac.kr>
2022-12-12 03:00:00 +0900

    그림맞추기 퍼즐 삽입 게임씬에

--- CookieHouse/Assets/Scripts/Puzzle/Piece.cs
+++ CookieHouse/Assets/Scripts/Puzzle/Piece.cs
@@ -2,21 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-public class Piece : MonoBehaviour
+using Fusion;
+public class Piece : NetworkBehaviour
 {
+    [Networked(OnChanged = nameof(setMatch))]
+    private bool doMatch { get; set; }
+
     [SerializeField] private Transform targetPosition;
     [SerializeField] private float distOffset;
-    public void CheckRightPosition()
+    private static void setMatch(Changed<Piece> changed)
     {
-        transform.SetParent(null);
-      
-        if (Vector3.Distance(targetPosition.position, this.transform.position) < distOffset)
+        changed.LoadNew();
+        if (changed.Behaviour.doMatch)
         {
-            transform.SetParent(targetPosition);
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-            transform.localScale = new Vector3(1, 1, 1);
-            transform.GetComponent<XRGrabInteractable>().enabled = false;
+            changed.Behaviour.gameObject.transform.SetParent(changed.Behaviour.targetPosition);
+            changed.Behaviour.gameObject.transform.localPosition = Vector3.zero;
+            changed.Behaviour.gameObject.transform.localRotation = Quaternion.identity;
+            changed.Behaviour.gameObject.transform.localScale = new Vector3(1, 1, 1);
+            changed.Behaviour.gameObject.transform.GetComponent<XrOffsetGrabInteractable>().enabled = false;
+            changed.Behaviour.doMatch = false;
+        }
+    }
+    public async void CheckRightPosition()
+    {
+        float distance = Vector3.Distance(targetPosition.position, this.transform.position);
+        if (distance < distOffset)
+        {
+            bool auth = await Object.WaitForStateAuthority();
+            transform.SetParent(null);
+            doMatch = true;
+        }
+        else if (distOffset < distance && distance < 3 * distOffset)
+        {
+            bool auth = await Object.WaitForStateAuthority();
+            this.transform.position += new Vector3(0.2f, 0, 0);
         }
     }
 }
+
+
